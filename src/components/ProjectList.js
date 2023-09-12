eafff356b89fcd2c3271d3f19ffdf5878fa75030
Rafael Moreira <rafa.siffert@gmail.com>
2023-09-12 14:40:27 -0400

    finished

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,16 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => (
+          <ProjectItem
+            key={project.id} // Make sure to provide a unique key
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies} // Pass the technologies prop
+          />
+        ))}
+      </div>
     </div>
   );
 }
